97d022e77ef9471e243c9dbe2abe3c9b0c611a66
spencerj <vorpehd@gmail.com>
2014-09-12 08:04:16 -0400

    Project
    .......

--- CheckLowRates/payroll/Program.cs
+++ CheckLowRates/payroll/Program.cs
@@ -10,8 +10,17 @@ namespace payroll
         static void Main(string[] args)
         {
             Console.Write("What Is Your Hourly Pay Rate? ");
+            Double HourlyPay = Convert.ToDouble(Console.ReadLine());
             Console.Write("How Many Hours Do You Work? ");
-            Double Hourly = Convert.ToDouble(Console.ReadLine());
+            Double Hours = Convert.ToDouble(Console.ReadLine());
+
+            Double grosspay = HourlyPay * Hours;
+            Double withholdingtax = grosspay / 100;
+            Double netpay = grosspay - withholdingtax;
+
+            Console.WriteLine("Gross Pay                  Withholding Tax");
+            Console.WriteLine("Up To And Including {0}   %{1}", grosspay, withholdingtax);
+            Console.WriteLine("{0} And Up                 %{1}", grosspay, withholdingtax++);
         }
     }
 }
